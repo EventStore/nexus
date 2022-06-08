eb9a4f42ae195775ada232796b0afa29ee8f5726
YoEight <yo.eight@gmail.com>
2022-06-08 22:51:43 +0200

    Move to new stackdriver client.

--- src/vector/sinks/metrics.rs
+++ src/vector/sinks/metrics.rs
@@ -1,10 +1,10 @@
 use futures::{stream::BoxStream, FutureExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use stackdriver_metrics::{Client, Options};
-use std::hash::{Hash, Hasher};
+use std::time::Duration;
 use vector::config::{DataType, SinkConfig, SinkContext, SinkDescription};
 use vector::event::{Event, MetricValue};
-use vector::sinks::gcp;
+use vector::sinks::gcp::{self, GcpTypedResource};
 use vector::sinks::util::StreamSink;
 use vector::sinks::{Healthcheck, VectorSink};
 
@@ -33,166 +33,128 @@ struct MetricSink {
     project_id: String,
     resource: gcp::GcpTypedResource,
     namespace: String,
-    started: chrono::DateTime<chrono::Utc>,
+    credential_path: Option<String>,
 }
 
 impl MetricSink {
     async fn new(
         project_id: String,
-        credentials_path: Option<String>,
+        credential_path: Option<String>,
         resource: gcp::GcpTypedResource,
         namespace: String,
     ) -> Result<Self, Box<dyn std::error::Error + Send + Sync + 'static>> {
-        let options = Options::default().credentials_options(credentials_path);
-
-        info!("gRPC Options: {:?}", options);
-        let client = Client::create(options).await?;
+        let client = Client::new().await?;
 
         Ok(Self {
             client,
             project_id,
             resource,
             namespace,
-            started: chrono::Utc::now(),
+            credential_path,
         })
     }
 }
 
-struct Wrap(stackdriver_metrics::TimeSeries);
-
-impl PartialEq for Wrap {
-    fn eq(&self, right: &Wrap) -> bool {
-        self.0.metric.r#type == right.0.metric.r#type
-    }
+struct Params {
+    default_namespace: String,
+    resource: GcpTypedResource,
+    started: chrono::DateTime<chrono::Utc>,
 }
 
-impl Eq for Wrap {}
+fn convert_event(params: &Params, event: Event) -> Option<stackdriver_metrics::TimeSeries> {
+    let metric = event.into_metric();
+    let namespace = metric
+        .series()
+        .name
+        .namespace
+        .as_ref()
+        .unwrap_or(&params.default_namespace);
 
-impl Hash for Wrap {
-    fn hash<H>(&self, state: &mut H)
-    where
-        H: Hasher,
-    {
-        self.0.metric.r#type.hash(state)
-    }
+    let metric_type = format!(
+        "custom.googleapis.com/{}/metrics/{}",
+        namespace,
+        metric.series().name.name
+    );
+    let metric_labels = metric
+        .series()
+        .tags
+        .clone()
+        .unwrap_or_default()
+        .into_iter()
+        .collect::<std::collections::HashMap<_, _>>();
+
+    let end_time = metric.data().timestamp.unwrap_or_else(chrono::Utc::now);
+
+    let (point_value, interval, metric_kind) = match &metric.data().value {
+        &MetricValue::Counter { value } => {
+            let interval = stackdriver_metrics::Interval {
+                start_time: Some(params.started),
+                end_time,
+            };
+
+            (value, interval, stackdriver_metrics::MetricKind::Cumulative)
+        }
+
+        &MetricValue::Gauge { value } => {
+            let interval = stackdriver_metrics::Interval {
+                start_time: None,
+                end_time,
+            };
+
+            (value, interval, stackdriver_metrics::MetricKind::Gauge)
+        }
+
+        not_supported => {
+            warn!("Unsupported metric type {:?}", not_supported);
+            return None;
+        }
+    };
+
+    let time_series = stackdriver_metrics::TimeSeries {
+        metric: stackdriver_metrics::TypedResource {
+            r#type: metric_type,
+            labels: metric_labels,
+        },
+
+        resource: stackdriver_metrics::TypedResource {
+            r#type: params.resource.r#type.clone(),
+            labels: params.resource.labels.clone(),
+        },
+
+        metric_kind,
+        value_type: stackdriver_metrics::ValueType::Int64,
+
+        points: stackdriver_metrics::Point {
+            interval,
+            value: stackdriver_metrics::PointValue {
+                int64_value: point_value as i64,
+            },
+        },
+    };
+
+    Some(time_series)
 }
-
-const MAX_GCP_SERIES_BATCH_SIZE: usize = 200;
 
 #[async_trait::async_trait]
 impl StreamSink for MetricSink {
-    async fn run(self: Box<Self>, mut input: BoxStream<'_, Event>) -> Result<(), ()> {
-        use std::time::{Duration, Instant};
+    async fn run(self: Box<Self>, input: BoxStream<'_, Event>) -> Result<(), ()> {
+        let options = Options::default()
+            .batch_size(200)
+            .period(Duration::from_secs(10))
+            .credentials_options(self.credential_path.clone());
 
-        let mut buffer = std::collections::HashSet::with_capacity(200);
-        let mut last_time = Instant::now();
+        let params = Params {
+            default_namespace: self.namespace.clone(),
+            resource: self.resource.clone(),
+            started: chrono::Utc::now(),
+        };
 
-        while let Some(event) = input.next().await {
-            let metric = event.into_metric();
-            let namespace = metric
-                .series()
-                .name
-                .namespace
-                .as_ref()
-                .unwrap_or(&self.namespace);
+        let stream =
+            input.filter_map(move |event| futures::future::ready(convert_event(&params, event)));
 
-            let metric_type = format!(
-                "custom.googleapis.com/{}/metrics/{}",
-                namespace,
-                metric.series().name.name
-            );
-            let metric_labels = metric
-                .series()
-                .tags
-                .clone()
-                .unwrap_or_default()
-                .into_iter()
-                .collect::<std::collections::HashMap<_, _>>();
-
-            let end_time = metric.data().timestamp.unwrap_or_else(chrono::Utc::now);
-
-            let (point_value, interval, metric_kind) = match &metric.data().value {
-                &MetricValue::Counter { value } => {
-                    let interval = stackdriver_metrics::Interval {
-                        start_time: Some(self.started),
-                        end_time,
-                    };
-
-                    (value, interval, stackdriver_metrics::MetricKind::Cumulative)
-                }
-
-                &MetricValue::Gauge { value } => {
-                    let interval = stackdriver_metrics::Interval {
-                        start_time: None,
-                        end_time,
-                    };
-
-                    (value, interval, stackdriver_metrics::MetricKind::Gauge)
-                }
-
-                not_supported => {
-                    warn!("Unsupported metric type {:?}", not_supported);
-                    continue;
-                }
-            };
-
-            let time_series = stackdriver_metrics::TimeSeries {
-                metric: stackdriver_metrics::TypedResource {
-                    r#type: metric_type,
-                    labels: metric_labels,
-                },
-
-                resource: stackdriver_metrics::TypedResource {
-                    r#type: self.resource.r#type.clone(),
-                    labels: self.resource.labels.clone(),
-                },
-
-                metric_kind,
-                value_type: stackdriver_metrics::ValueType::Int64,
-
-                points: stackdriver_metrics::Point {
-                    interval,
-                    value: stackdriver_metrics::PointValue {
-                        int64_value: point_value as i64,
-                    },
-                },
-            };
-
-            let wrapped = Wrap(time_series);
-            if buffer.contains(&wrapped) {
-                buffer.replace(wrapped);
-            } else {
-                buffer.insert(wrapped);
-            }
-
-            if buffer.len() == MAX_GCP_SERIES_BATCH_SIZE
-                || last_time.elapsed() >= Duration::from_secs(10)
-            {
-                let values = buffer.drain().map(|w| w.0).collect();
-
-                if let Err(e) = self
-                    .client
-                    .create_time_series(self.project_id.as_str(), values)
-                    .await
-                {
-                    match e {
-                        stackdriver_metrics::Error::Grpc(status) => {
-                            warn!("gRPC error happened: {:?}", status);
-                        }
-
-                        stackdriver_metrics::Error::InvalidArgument(e) => {
-                            error!("GCP configuration error: {}", e);
-                            break;
-                        }
-                    }
-                } else {
-                    info!("Successfully pushed time series!");
-                }
-
-                last_time = Instant::now();
-            }
-        }
-
+        self.client
+            .stream_time_series(self.project_id.as_str(), &options, Box::pin(stream))
+            .await;
         Ok(())
     }
 }
